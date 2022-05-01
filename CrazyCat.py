792988f7d855881ea19ea8a866fc55b7367e9ac9
Joshua Ramos <78503965+jramos823@users.noreply.github.com>
2022-05-01 02:03:25 -0400

    Creation of file
    Added code

---
+++ CrazyCat.py
@@ -0,0 +1,16 @@
+#Cat displays message 
+
+def crazycat():
+    userinput = input("Enter for cat to say it! ")
+    topborder = "_"
+    bottomborder = "-"
+    print("          {}".format(topborder * len(userinput)))
+    print("        < {} >".format(userinput))
+    print("          {}".format(bottomborder * len(userinput)))
+    print("         /")
+    print(" /\_/\  /") 
+    print("( o.o )") 
+    print(" > ^ <") 
+
+
+crazycat()
